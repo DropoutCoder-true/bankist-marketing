8bb863ed55254754d6bbfacbbdb35d5ae0640355
vighnesh <vighneshzagade4@gmail.com>
2024-07-24 15:49:13 +0530

    sticky-navbar-js

--- script.js
+++ script.js
@@ -89,7 +89,7 @@ tabsContainer.addEventListener("click", function(e){
 
 // Nav hover animation
 const handleHover = function(e){
-  console.log(this, e.currentTarget); 
+  // console.log(this, e.currentTarget); 
   if(e.target.classList.contains("nav__link")){
     const link = e.target; 
     const siblings = link.closest(".nav").querySelectorAll(".nav__link");
@@ -105,3 +105,13 @@ const handleHover = function(e){
 
 nav.addEventListener("mouseover", handleHover.bind(0.5)); 
 nav.addEventListener("mouseout", handleHover.bind(1)); 
+
+// Sticky navigation 
+const initialCoords = section1.getBoundingClientRect(); 
+// console.log(initialCoords); 
+
+window.addEventListener("scroll", function(e){
+  console.log(window.scrollY); 
+  if(window.scrollY > initialCoords.top) nav.classList.add("sticky"); 
+  else nav.classList.remove("sticky"); 
+})
